2f69638b36d564270b8bd66c46862c6269a45711
Akshay Lokray <akshay_lr@Akshays-MacBook-Pro.local>
2019-12-26 22:42:47 +0530

    Updates

---
+++ precache-manifest.183d32af645b67249df606420f31d89e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a816bf7936b11487c9a857bad593561a",
+    "url": "/flickr/index.html"
+  },
+  {
+    "revision": "b68988ca97275bd7a519",
+    "url": "/flickr/static/css/main.c921d3bc.chunk.css"
+  },
+  {
+    "revision": "721874c2161e252749e7",
+    "url": "/flickr/static/js/2.e656b439.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/flickr/static/js/2.e656b439.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b68988ca97275bd7a519",
+    "url": "/flickr/static/js/main.0457d6a5.chunk.js"
+  },
+  {
+    "revision": "88fc341d0b4f858292c1",
+    "url": "/flickr/static/js/runtime-main.07c72c6b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0de73b4cf9a921bf4c41213c7bb0b661.js"
+  "/flickr/precache-manifest.183d32af645b67249df606420f31d89e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/flickr/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
