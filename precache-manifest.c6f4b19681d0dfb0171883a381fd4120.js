6455b1e03913401ed0acd183dc1a9d6347f52ab4
Akshay Lokray <akshay_lr@Akshays-MacBook-Pro.local>
2019-12-26 20:11:18 +0530

    Updates

---
+++ precache-manifest.c6f4b19681d0dfb0171883a381fd4120.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba7fd7113eb7357719a2b48e5d6ec990",
+    "url": "/flickr/index.html"
+  },
+  {
+    "revision": "f78abaf71ace49e9e491",
+    "url": "/flickr/static/css/main.0ca7a17a.chunk.css"
+  },
+  {
+    "revision": "721874c2161e252749e7",
+    "url": "/flickr/static/js/2.e656b439.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/flickr/static/js/2.e656b439.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f78abaf71ace49e9e491",
+    "url": "/flickr/static/js/main.27b6d3a1.chunk.js"
+  },
+  {
+    "revision": "88fc341d0b4f858292c1",
+    "url": "/flickr/static/js/runtime-main.07c72c6b.js"
+  }
+]);
